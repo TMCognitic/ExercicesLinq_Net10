a4fa4ff9a9435d7fafa5d0c74f2286509f6533a2
Morre Thierry <thierry.morre@cognitic.be>
2026-03-25 09:24:32 +0100

    Initialize DataContext

---
+++ ExercicesLinq/Program.cs
@@ -0,0 +1,16 @@
+﻿using Domain;
+
+namespace ExercicesLinq;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        DataContext dc = new DataContext();
+
+        foreach (Student student in dc.Students)
+        {
+            Console.WriteLine(student.Last_Name);
+        }
+    }
+}
